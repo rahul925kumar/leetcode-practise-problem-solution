0938934ff1014d0421db6cec1cc9e97a105faab8
rahul925kumar <rahulpassi925@gmail.com>
2022-09-17 19:16:44 +0530

    -

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # leetcode-practise-problem-solution
 LeetCode Problem's Solution
+
+# Find First and Last Position of Element in Sorted Array
--- find_firstandlast_position_of_element_in_sorted_array.js
+++ find_firstandlast_position_of_element_in_sorted_array.js
@@ -4,6 +4,7 @@
  * Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
  If target is not found in the array, return [-1, -1].
  You must write an algorithm with O(log n) runtime complexity.
+
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
