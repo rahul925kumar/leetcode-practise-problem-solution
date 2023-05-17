310931e878d1a89b0680a5a48969e6b066b0b3c0
rahul925kumar <rahulpassi925@gmail.com>
2023-05-17 14:21:24 +0530

    --

---
+++ SQL/577. Employee Bonus.sql
@@ -0,0 +1,36 @@
+"Write an SQL query to report the name and bonus amount of each employee with a bonus less than 1000.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-------+--------+------------+--------+
+| empId | name   | supervisor | salary |
++-------+--------+------------+--------+
+| 3     | Brad   | null       | 4000   |
+| 1     | John   | 3          | 1000   |
+| 2     | Dan    | 3          | 2000   |
+| 4     | Thomas | 3          | 4000   |
++-------+--------+------------+--------+
+Bonus table:
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+Output: 
++------+-------+
+| name | bonus |
++------+-------+
+| Brad | null  |
+| John | null  |
+| Dan  | 500   |
++------+-------+"
+select Employee.name, Bonus.bonus from Employee left outer join Bonus on Employee.empId = Bonus.empId where bonus < 1000 OR bonus is null;
